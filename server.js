198966964269df4e39ef13304bf39319921a4f49
Gruppen <kolodiva@mail.ru>
2019-05-30 16:37:24 +0300

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const path 	= require('path');
 // ==============================================
 app.use(cors());
 
-app.use(express.static( __dirname + '/public') );
+app.use(express.static('public'));
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
