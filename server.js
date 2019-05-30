46728795512670010166af10187346595490d1e6
Gruppen <kolodiva@mail.ru>
2019-05-30 17:47:27 +0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var pg = require('pg');
 
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+
 var express = require('express');
 var cors 	= require('cors');
 var favicon = require('serve-favicon')
@@ -8,29 +12,36 @@ var port    =   process.env.PORT || 8080;
 
 var router 	= express.Router();
 
-const path 	= require('path');
+var path 	= require('path');
 
 // ROUTES
 // ==============================================
+
+//app.use('public', express.static(__dirname + '/public'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(cors());
 
-app.use('/public', express.static(__dirname + '/public'));
- 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/dist')));
+//app.use(express.static(path.join(__dirname, '/dist')));
 
-router.get('/', function(req, res) {
-    res.sendFile('index.html');
-});
+// router.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/public/favicon.ico');
+// });
 
 // about page route (http://localhost:8080/about)
 router.get('/about', function(req, res) {
     res.send('im the about page und Redux ---<<->>----->'); 
 });
 
+router.get('/text', function(req, res) {
+    res.send( req.cookies ); 
+});
 
 // apply the routes to our application
 app.use('/', router);
